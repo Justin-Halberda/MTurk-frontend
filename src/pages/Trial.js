ba265c18b8ce92c11aef7240efc1817a3eae3261
fuaad001 <husseinfuaad@hotmail.com>
2023-02-07 16:08:50 -0500

    Solved minor error

--- src/pages/Trial.js
+++ src/pages/Trial.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Image from '../components/Image'
 
 const imgStyles = [
-    {style: { left:"5%", top: "7%", writable: true}, src: "BFH/BFH" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "10%", top: "60%"}, src: "BFL/BFL" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "30%", top: "20%"}, src: "BMH/BMH" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "35%", top: "75%"}, src: "BML/BML" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "53%", top: "50%"}, src: "WFH/WFH" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "65%", top: "15%"}, src: "WFL/WFL" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "75%", top: "65%"}, src: "WMH/WMH" + Math.floor(Math.random() * 10) + ".jpg"},
-    {style: { left: "85%", top: "30%"}, src: "WML/WML" + Math.floor(Math.random() * 10) + ".jpg"},
+    {style: { left:"5%", top: "7%", writable: true}, src: "BFH/BFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "10%", top: "60%"}, src: "BFL/BFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "30%", top: "20%"}, src: "BMH/BMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "35%", top: "75%"}, src: "BML/BML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "53%", top: "50%"}, src: "WFH/WFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "65%", top: "15%"}, src: "WFL/WFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "75%", top: "65%"}, src: "WMH/WMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "85%", top: "30%"}, src: "WML/WML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
 ];
 
 const categories = ["BFH", "BFL", "BMH", "BML", "WHF", "WFL", "WMH", "WML"];
@@ -19,7 +19,7 @@ export default function Trial() {
     const [ origSrc, setOrigSrc ] = useState(imgStyles[origImg].src);
     const [ blank, setBlank ] = useState(true);
     const [ newImgCat, setNewImgCat ] = useState(categories[Math.floor(Math.random() * 8)]);
-    const [ newImg, setNewImg ] = useState(Math.floor(Math.random() * 10));
+    const [ newImg, setNewImg ] = useState((Math.floor(Math.random() * 10) + 1));
     const [ spaceKey, setSpaceKey ] = useState(false);
     const [ startTime, setStartTime ] = useState(Date.now());
     const [ payload, setPayload ] = useState({
@@ -32,7 +32,11 @@ export default function Trial() {
 
     while (origSrc.includes(categories[newImgCat] + "/" + newImg)) {
         setNewImgCat(categories[Math.floor(Math.random() * 8)]);
-        setNewImg(Math.floor(Math.random() * 10));
+        setNewImg((Math.floor(Math.random() * 10) + 1));
+        setPayload({
+            ...payload,
+            changing_after: newImgCat + "/" + newImgCat + newImg + ".jpg",
+        });
     }
 
     useEffect(() => {
@@ -61,7 +65,6 @@ export default function Trial() {
                 time: duration
             });
             setSpaceKey(true);
-            setBlank(false);
         }
     }
 
