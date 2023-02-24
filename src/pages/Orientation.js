f15af031f945950b2cd2d0366b57e42fc89cdad6
fuaad001 <husseinfuaad@hotmail.com>
2023-02-24 09:23:52 -0500

    orientation angles updated

--- src/pages/Orientation.js
+++ src/pages/Orientation.js
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 import store from '../utils/store';
 
 const imgStyles = [
-    {style: { left:"5%", top: "7%", width: "80px", height: "170px", writable: true}, src: "1"},
-    {style: { left: "10%", top: "60%", width: "100px", height: "170px"}, src: "3"},
-    {style: { left: "30%", top: "20%", width: "150px", height: "100px"}, src: "5"},
-    {style: { left: "35%", top: "75%", width: "100px", height: "170px"}, src: "6"},
-    {style: { left: "53%", top: "50%", width: "40px", height: "150px"}, src: "2"},
-    {style: { left: "65%", top: "15%", width: "40px", height: "150px"}, src: "4"},
-    {style: { left: "75%", top: "65%", width: "80px", height: "170px"}, src: "7"},
+    {style: { left:"5%", top: "7%", width: "100px", height: "170px", writable: true}, src: "1"},
+    {style: { left: "10%", top: "60%", width: "170px", height: "170px"}, src: "3"},
+    {style: { left: "30%", top: "20%", width: "170px", height: "100px"}, src: "5"},
+    {style: { left: "35%", top: "75%", width: "170px"}, src: "6"},
+    {style: { left: "53%", top: "50%", width: "170px", height: "50px"}, src: "2"},
+    {style: { left: "65%", top: "15%", width: "50px", height: "165px"}, src: "4"},
+    {style: { left: "75%", top: "65%", width: "90px", height: "175px"}, src: "7"},
     {style: { left: "85%", top: "30%", width: "175px", height: "90px"}, src: "8"},
 ];
 
