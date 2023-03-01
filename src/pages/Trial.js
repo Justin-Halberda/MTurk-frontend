f4aa69fe2782a816a1d4c672c2d5eb5f85d44444
fuaad001 <husseinfuaad@hotmail.com>
2023-03-01 13:02:56 -0500

    Fixed slow loading image problem

--- src/pages/Trial.js
+++ src/pages/Trial.js
@@ -3,15 +3,15 @@ import Image from '../components/Image'
 import { useNavigate } from "react-router-dom";
 import store from '../utils/store';
 
-const imgStyles = [
-    {style: { left:"5%", top: "7%", writable: true}, src: "BFH/BFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "10%", top: "60%"}, src: "BFL/BFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "30%", top: "20%"}, src: "BMH/BMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "35%", top: "75%"}, src: "BML/BML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "53%", top: "50%"}, src: "WFH/WFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "65%", top: "15%"}, src: "WFL/WFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "75%", top: "65%"}, src: "WMH/WMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
-    {style: { left: "85%", top: "30%"}, src: "WML/WML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+let imgStyles = [
+    {style: { left:"5%", top: "7%", display: "block", writable: true}, src: "BFH/BFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "10%", top: "60%", display: "block"}, src: "BFL/BFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "30%", top: "20%", display: "block"}, src: "BMH/BMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "35%", top: "75%", display: "block"}, src: "BML/BML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "53%", top: "50%", display: "block"}, src: "WFH/WFH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "65%", top: "15%", display: "block"}, src: "WFL/WFL" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "75%", top: "65%", display: "block"}, src: "WMH/WMH" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
+    {style: { left: "85%", top: "30%", display: "block"}, src: "WML/WML" + (Math.floor(Math.random() * 10) + 1) + ".jpg"},
 ];
 
 const categories = ["BFH", "BFL", "BMH", "BML", "WFH", "WFL", "WMH", "WML"];
@@ -32,6 +32,8 @@ export default function Trial() {
         images_used: imgStyles.map((style, key) => { return style.src })
     });
 
+    imgStyles.push({style: Object.assign({}, imgStyles[origImg].style, { display: "none" }), src: newImgCat + "/" + newImgCat + newImg + ".jpg"});
+
     while (origSrc.includes(categories[newImgCat] + "/" + newImg)) {
         setNewImgCat(categories[Math.floor(Math.random() * 8)]);
         setNewImg((Math.floor(Math.random() * 10) + 1));
@@ -39,6 +41,8 @@ export default function Trial() {
             ...payload,
             changing_after: newImgCat + "/" + newImgCat + newImg + ".jpg",
         });
+        imgStyles[8].style = Object.assign({}, imgStyles[origImg].style, { display: "none" });
+        imgStyles[8].src = newImgCat + "/" + newImgCat + newImg + ".jpg";
     }
 
     const navigate = useNavigate();
@@ -58,8 +62,13 @@ export default function Trial() {
         const interval = setInterval(() => {
             setBlank(!blank);
             if (blank) { 
-                let temp = newImgCat + "/" + newImgCat + newImg + ".jpg";
-                imgStyles[origImg].src === origSrc ? imgStyles[origImg].src = temp : imgStyles[origImg].src = origSrc;
+                if (imgStyles[8].style.display === "none") {
+                    imgStyles[8].style = Object.assign({}, imgStyles[8].style, { display: "block" });
+                    imgStyles[origImg].style = Object.assign({}, imgStyles[origImg].style, { display: "none" });
+                } else {
+                    imgStyles[8].style = Object.assign({}, imgStyles[8].style, { display: "none" });
+                    imgStyles[origImg].style = Object.assign({}, imgStyles[origImg].style, { display: "block" });
+                }
             }
 
         }, 1000);
@@ -78,7 +87,11 @@ export default function Trial() {
         }
     }
 
-    return (blank ? <div></div> : imgStyles.map((img, key) => (
-        <Image spaceKey = {spaceKey} style = {img.style} src = {img.src} payload = {payload} type={"trial"}/>
-    )));
+    return (
+        <div style={blank ? {display: "none"} : {}}>
+            {imgStyles.map((img, key) => (
+                <Image spaceKey = {spaceKey} style = {img.style} src = {img.src} payload = {payload} type={"trial"}/>
+            ))}
+        </div> 
+    );
 }
